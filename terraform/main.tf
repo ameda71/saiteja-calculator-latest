ec32df651dc731a05f07288ae7d05c59a8c5c8cd
saicharan <141946357+Saicharan619@users.noreply.github.com>
2025-04-24 18:40:55 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,29 +1,27 @@
 // Configure the Google Cloud provider with project and region
 provider "google" {
-  project = var.project_id  // GCP project ID
-  region  = var.region      // GCP region for the cluster
+  project = var.project_id
+  region  = var.region
 }
 
 // Create a Google Kubernetes Engine (GKE) cluster
 resource "google_container_cluster" "primary" {
-  name     = var.cluster_name // Cluster name
-  location = var.zone      // GKE zone
-}         
+  name     = var.cluster_name
+  location = var.zone
 
-  initial_node_count = var.initial_node_count  // Number of nodes in the cluster
+  initial_node_count = var.initial_node_count
 
   node_config {
-    machine_type = var.node_machine_type  // Node machine type (e.g., e2-medium)
+    machine_type = var.node_machine_type
     disk_size_gb = 20
   }
 }
- 
+
 // Output the cluster's name and endpoint
-output "CLUSTER_NAME" {
+output "cluster_name" {
   value = google_container_cluster.primary.name
 }
 
 output "cluster_endpoint" {
   value = google_container_cluster.primary.endpoint
 }
-
